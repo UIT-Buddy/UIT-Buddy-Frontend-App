b73f19b66789f34b08bc80a88b9b7893db281867
UIT.23520540 <23520540@gm.uit.edu.vn>
2026-05-25 06:42:11 +0700

    chore: baseurl

--- lib/core/config/app_env.dart
+++ lib/core/config/app_env.dart
@@ -1,7 +1,7 @@
 class AppEnv {
   static const String baseUrl = String.fromEnvironment(
     'BASE_URL',
-    defaultValue: 'http://52.64.199.49:8080',
+    defaultValue: 'http://52.64.199.49/buddy',
   );
 
   static const String weatherApiKey = String.fromEnvironment('WEATHER_API_KEY');
